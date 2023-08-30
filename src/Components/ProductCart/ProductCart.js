6be7ab4ac45b8db9d6b64f5a35c7e17b45538ec3
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-08-30 15:52:06 +0300

    useState useNavigate Axios take photos from server

--- src/Components/ProductCart/ProductCart.js
+++ src/Components/ProductCart/ProductCart.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './ProductCart.module.scss';
+import { Card } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const ProductCart = ({product}) => {
+
+  const navigate = useNavigate();
+
   return (
     <div>
-      {product.title}
+      <Card
+      style={{ width : '18rem', textAlign: 'center' }}
+      className={styles.productCard}>
+      <Card.Img onClick={() => navigate(`/products/${product.id}`)}
+      variant='top'
+      src={product?.productPhoto}
+      className={styles.cardImg}/>
+
+      </Card>
        
     </div>
   )
