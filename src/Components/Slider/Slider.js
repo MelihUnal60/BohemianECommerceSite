44d84a5acf4b3244d05d3bb9510e67986f0f499a
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-08-31 00:28:35 +0300

    Main slider revisions

---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import img1 from '../../assets/img1.webp';
+import img2 from '../../assets/img2.webp';
+import img3 from '../../assets/img3.webp';
+import { Carousel, CarouselItem } from 'react-bootstrap';
+import styles from './Slider.module.scss';
+
+const Slider = () => {
+
+    const sliderItems = [
+        {
+            id: 1,
+            caption: 'El Yapımı Tabaklar Burada',
+            img: img1
+        },
+        {
+            id: 2,
+            caption: 'Güncel Ürünler',
+            img: img2
+        },
+        {
+            id: 3,
+            caption: 'Çok Satanlar',
+            img: img3
+        }
+    ]
+
+  return (
+    <div>
+        <Carousel fade>
+            {
+                sliderItems.map((item) => {
+                    return(
+                        <CarouselItem key={item.id} className={styles.sliderItem}>
+
+                        </CarouselItem>
+                    )
+                })
+            }
+        </Carousel>
+    </div>
+  )
+}
+
+export default Slider;
